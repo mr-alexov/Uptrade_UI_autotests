7aecfae52684b8a352e9957bc37cccaf752a9ef3
Alex Ov <alexov@swisscows.email>
2024-12-28 00:10:38 +0200

    Add more logging

--- src/test/java/config/ConfigRunner.java
+++ src/test/java/config/ConfigRunner.java
@@ -3,11 +3,15 @@ package config;
 import com.codeborne.selenide.Configuration;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class ConfigRunner {
     private final WebDriverConfig config;
+
+    Logger logger = LoggerFactory.getLogger(ConfigRunner.class);
 
     public ConfigRunner() {
         this.config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
@@ -17,8 +21,18 @@ public class ConfigRunner {
     private void createDriver() {
 
         Configuration.browser = config.getBrowserName();
+
+        logger.info("Browser: " + Configuration.browser);
+
         Configuration.browserVersion = config.getBrowserVersion();
+
+        logger.info("Browser version: " + Configuration.browserVersion);
+
         Configuration.browserSize = config.getBrowserSize();
+
+        logger.info("Browser size: " + Configuration.browserSize);
+
+        logger.info("Remote URL: " + config.getRemoteURL());
 
         if (config.getRemoteURL() != null) {
             Configuration.remote = config.getRemoteURL().toString();
